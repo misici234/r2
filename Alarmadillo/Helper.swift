ba165b6f46f4e06d6f2b009aa2f0386cae28d51e
misici234 <misici234@yahoo.ca>
2019-04-23 17:23:09 -0400

    added unnecessery blank lines to Helper.swift file

--- Alarmadillo/Helper.swift
+++ Alarmadillo/Helper.swift
@@ -9,10 +9,12 @@
 import Foundation
 
 struct Helper {
+
 	static func getDocumentsDirectory() -> URL {
 		let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
 		let documentsDirectory = paths[0]
 
 		return documentsDirectory
 	}
+
 }
